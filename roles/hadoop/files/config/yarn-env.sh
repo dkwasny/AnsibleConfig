81cb10e91c2ec6336aa66e1f573da98cf69fbd61
dkwasny <dkwasny67@gmail.com>
2015-05-29 16:46:51 -0400

    Dropping YARN, Hadoop and HBase heap sizes to 512m.

--- roles/hadoop/files/config/yarn-env.sh
+++ roles/hadoop/files/config/yarn-env.sh
@@ -37,7 +37,7 @@ JAVA_HEAP_MAX=-Xmx1000m
 
 # For setting YARN specific HEAP sizes please use this
 # Parameter and set appropriately
-# YARN_HEAPSIZE=1000
+YARN_HEAPSIZE=512
 
 # check envvars which might override default args
 if [ "$YARN_HEAPSIZE" != "" ]; then
