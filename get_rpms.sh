ac3a382fc1e24f7038037256abf8882aad465c1b
dkwasny <dkwasny67@gmail.com>
2015-06-12 21:45:25 -0400

    Adding hive to get_rpms.sh

--- get_rpms.sh
+++ get_rpms.sh
@@ -20,3 +20,4 @@ get_file "https://www.dropbox.com/s/t8eoculloa15c5u/solr-5.0.0-1.fc21.noarch.rpm
 get_file "https://www.dropbox.com/s/ax5pax6bp9m6vw4/hadoop-2.6.0-1.fc21.x86_64.rpm?dl=1" "$BASE_DIR/roles/hadoop/files/hadoop-2.6.0-1.fc21.x86_64.rpm";
 get_file "https://www.dropbox.com/s/dnt92fy0pemhkji/hbase-1.0.0-1.fc21.x86_64.rpm?dl=1" "$BASE_DIR/roles/hbase/files/hbase-1.0.0-1.fc21.x86_64.rpm";
 get_file "https://www.dropbox.com/s/q11f80l422b3f4g/pig-0.14.0-1.fc21.noarch.rpm?dl=1" "$BASE_DIR/roles/pig/files/pig-0.14.0-1.fc21.noarch.rpm";
+get_file "https://www.dropbox.com/s/gqy6pcnfpcoq1ms/hive-1.2.0-1.el7.centos.noarch.rpm?dl=1" "$BASE_DIR/roles/hive/files/hive-1.2.0-1.el7.centos.noarch.rpm";
