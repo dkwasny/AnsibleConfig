4919fb35808d848d9fbcaaf5fb1b3529e3d6fc48
dkwasny <dkwasny67@gmail.com>
2015-11-21 00:30:42 -0500

    Fixing up the admin_user role

--- roles/admin_user/files/home/hdfs-format.sh
+++ roles/admin_user/files/home/hdfs-format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u hdfs -E /opt/hadoop/bin/hdfs namenode -format;
+sudo -u hdfs hdfs namenode -format;
---
+++ roles/admin_user/files/home/hdfs-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Some commands I have to run whenever starting with a fresh HDFS.
+# I'm probably doing something wrong here...
+
+sudo -u hdfs hdfs dfs -chown hdfs:hdfs /tmp;
+sudo -u hdfs hdfs dfs -chmod 777 /tmp;
+sudo -u hdfs hdfs dfs -chmod -R 777 /yarn;
